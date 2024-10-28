ffe24b86bf105eaa20446773899ec3e9a533f580
josefVrba <josefvrba.ol18@gmail.com>
2024-10-28 15:49:41 +0100

    updated qtile

--- qtile/config.py
+++ qtile/config.py
@@ -36,51 +36,52 @@ def autostart():
     script = os.path.expanduser("~/.config/qtile/autostart.sh")
     subprocess.Popen([script])
 
-mod = "mod4"
+alt_key = "mod1"
+super_key = "mod4"
 terminal = "kitty"
 
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([alt_key], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([alt_key], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([alt_key], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([alt_key], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([alt_key], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([alt_key, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([alt_key, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([alt_key, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([alt_key, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([alt_key, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([alt_key, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([alt_key, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([alt_key, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([alt_key], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    #Key( [mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack",),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    #Key( [alt_key, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack",),
+    Key([super_key], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([alt_key], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([alt_key], "w", lazy.window.kill(), desc="Kill focused window"),
     Key(
-        [mod],
+        [alt_key],
         "f",
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod, "shift"], "Return", lazy.spawn("rofi -show drun")),
+    Key([alt_key], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([alt_key, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([alt_key, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([alt_key], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([alt_key, "shift"], "Return", lazy.spawn("rofi -show drun")),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -104,7 +105,7 @@ for i in groups:
         [
             # mod + group number = switch to group
             Key(
-                [mod],
+                [alt_key],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc=f"Switch to group {i.name}",
